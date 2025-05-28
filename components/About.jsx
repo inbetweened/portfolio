f67b89fc03956c4df577bba16427bd520c9ac706
Daniel <simmendaniel6@gmail.com>
2025-05-28 15:26:22 +0200

    Update About.jsx
    Anpassen

--- components/About.jsx
+++ components/About.jsx
@@ -5,9 +5,7 @@ export default function About({ aboutRef }) {
     <section id="aboutme" ref={aboutRef} className={styles.about}>
       <h2 className={styles.heading}>About Me</h2>
       <p className={styles.text}>
-        I’m Daniel, a creative mind with a passion for animation, motion graphics
-        and interactive design. I love creating dynamic, visual stories that feel
-        alive.
+        Hallo ich bin Dani
       </p>
     </section>
   );
