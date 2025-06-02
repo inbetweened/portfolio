d5ef68cf7e0456b77c23531ab90672ad1d5f7d25
Daniel <simmendaniel6@gmail.com>
2025-06-02 21:48:18 +0200

    Create about.jsx

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">About Me</h1>
+      <p>
+        I'm Dani – I design, animate and build digital experiences.
+        This is where you can write your bio, skills, tools you use etc.
+      </p>
+    </div>
+  );
+}
