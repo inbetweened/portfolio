d3be89d16ab52596504f95ab5cac71a319ee63d5
Daniel <simmendaniel6@gmail.com>
2025-06-02 22:11:39 +0200

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,13 +1,18 @@
-import SeoHead from '../components/SeoHead';
-import Navbar from '../components/Navbar';
-import Work from '../components/Work';
+import styles from '../styles/Work.module.css';
 
-export default function WorkPage() {
+export default function Work({ cardsRef }) {
   return (
-    <>
-      <SeoHead title="Work – Daniel" description="A collection of my favorite projects and case studies." />
-      <Navbar />
-      <Work />
-    </>
+    <section id="work" className={styles.work}>
+      <h2 className={styles.heading}>Work</h2>
+      <div className={styles.grid}>
+        {[0, 1, 2].map((_, index) => (
+          <div
+            key={index}
+            ref={(el) => (cardsRef.current[index] = el)}
+            className={styles.card}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
